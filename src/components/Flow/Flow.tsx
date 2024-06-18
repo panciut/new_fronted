9c572fc0486d9f2a9d679c6c75a2d775466fd6a5
panciut <pancimarco02@gmail.com>
2024-06-18 15:43:37 +0200

    graph automatically ordered

--- src/components/Flow/Flow.tsx
+++ src/components/Flow/Flow.tsx
@@ -1,12 +1,19 @@
 // src/components/Flow/Flow.tsx
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import ReactFlow, { Background, Controls, MiniMap, Node, Edge, useNodesState, useEdgesState, addEdge, Connection } from 'react-flow-renderer';
-import { FlowContainer } from './Flow.styles';
+import dagre from 'dagre';
 
 interface FlowProps {
   initialNodes: Node[];
   initialEdges: Edge[];
 }
+
+const nodeWidth = 172;
+const nodeHeight = 36;
+
+const dagreGraph = new dagre.graphlib.Graph();
+dagreGraph.setDefaultEdgeLabel(() => ({}));
+dagreGraph.setGraph({ rankdir: 'TB' });
 
 const Flow: React.FC<FlowProps> = ({ initialNodes, initialEdges }) => {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
@@ -14,8 +21,32 @@ const Flow: React.FC<FlowProps> = ({ initialNodes, initialEdges }) => {
 
   const onConnect = useCallback((params: Edge | Connection) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
 
+  useEffect(() => {
+    // Layout nodes using dagre
+    initialNodes.forEach((node) => {
+      dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
+    });
+
+    initialEdges.forEach((edge) => {
+      dagreGraph.setEdge(edge.source, edge.target);
+    });
+
+    dagre.layout(dagreGraph);
+
+    const layoutedNodes = initialNodes.map((node) => {
+      const nodeWithPosition = dagreGraph.node(node.id);
+      node.position = {
+        x: nodeWithPosition.x - nodeWidth / 2,
+        y: nodeWithPosition.y - nodeHeight / 2,
+      };
+      return node;
+    });
+
+    setNodes(layoutedNodes);
+  }, [initialNodes, initialEdges]);
+
   return (
-    <FlowContainer>
+    <div style={{ height: 600, width: '100%' }}>
       <ReactFlow
         nodes={nodes}
         edges={edges}
@@ -28,7 +59,7 @@ const Flow: React.FC<FlowProps> = ({ initialNodes, initialEdges }) => {
         <Controls />
         <Background />
       </ReactFlow>
-    </FlowContainer>
+    </div>
   );
 };
 
